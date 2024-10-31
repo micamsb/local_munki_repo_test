fb775efd914cc8b61a3796a06e1289d088b17616
micamsb <mica.schuepbach@unibas.ch>
2024-10-31 16:37:57 +0100

    Server shutdown function

--- revert_changes.sh
+++ revert_changes.sh
@@ -4,11 +4,22 @@
 
 
 #   functions   #
+function stop_apachectl (){                                                    # stops apachectl server
+    launchctl print system/org.apache.httpd &> /dev/null
+
+    if [ $? -eq 1 ]; then
+        sleep .2
+    else 
+        sudo apachectl stop
+    fi
+}
+
 function revert_munki_repo_preferences (){
     defaults write com.github.autopkg MUNKI_REPO /volumes/files/html/munki_repo_dev
 }
 
 #   script   #
+stop_apachectl
 revert_munki_repo_preferences &> /dev/null
 
 
